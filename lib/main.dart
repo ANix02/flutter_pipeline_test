ce62af786d14dd03fb98d68dfe21018260b403f6
ape <67485870+ANix02@users.noreply.github.com>
2024-02-23 22:28:56 +0100

    Add package_info_plus and http dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,26 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+
 import 'package:flutter/material.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+import 'package:updat/updat_window_manager.dart';
 
 void main() async {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  PackageInfo packageInfo = await PackageInfo.fromPlatform();
+
+  String version = packageInfo.version;
+  runApp(MyApp(version: version));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final String version;
+  const MyApp({
+    Key? key,
+    required this.version,
+  }) : super(key: key);
 
   // This widget is the root of your application.
   @override
@@ -31,8 +46,59 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: UpdatWindowManager(
+        currentVersion:
+            version, // In this case we are importing it form a constants file.
+        getLatestVersion: () async {
+          // Github gives us a super useful latest endpoint, and we can use it to get the latest stable release
+          final data = await http.get(Uri.parse(
+            "https://api.github.com/repos/ANix02/flutter_pipeline_test/releases/latest",
+          ));
+
+          // Return the tag name, which is always a semantically versioned string.
+          return jsonDecode(data.body)["tag_name"];
+        },
+        getBinaryUrl: (version) async {
+          // Github also gives us a great way to download the binary for a certain release (as long as we use a consistent naming scheme)
+
+          // Make sure that this link includes the platform extension with which to save your binary.
+          // If you use https://exapmle.com/latest/macos for instance then you need to create your own file using `getDownloadFileLocation`
+          return "https://github.com/ANix02/flutter_pipeline_test/releases/download/$version/updat-${Platform.operatingSystem}-$version.$platformExt";
+        },
+        appName: "Updat Example", // This is used to name the downloaded files.
+        getChangelog: (_, __) async {
+          // That same latest endpoint gives us access to a markdown-flavored release body. Perfect!
+          final data = await http.get(Uri.parse(
+            "https://api.github.com/repos/ANix02/flutter_pipeline_test/releases/latest",
+          ));
+          return jsonDecode(data.body)["body"];
+        },
+        child: const MyHomePage(title: 'Flutter Demo Home Page'),
+      ),
     );
+  }
+
+  String get platformExt {
+    switch (Platform.operatingSystem) {
+      case 'windows':
+        {
+          return 'msix';
+        }
+
+      case 'macos':
+        {
+          return 'dmg';
+        }
+
+      case 'linux':
+        {
+          return 'AppImage';
+        }
+      default:
+        {
+          return 'zip';
+        }
+    }
   }
 }
 
